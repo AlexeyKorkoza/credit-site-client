8994537cfef50725c4760d4a91bb34a9f7319e62
AlexeyKorkoza <alexeykorkoza@gmail.com>
2020-05-07 23:36:28 +0300

    refactor: modify webpack, add lazy & variables to config

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,22 @@
-import React, { useContext } from 'react';
+import React, { lazy, useContext, Suspense } from 'react';
 import { Switch, Route } from 'react-router-dom';
 import ReactNotification from 'react-notifications-component';
 
 import { GlobalStyle, Page } from './styles';
 import { AuthRoleRoute, AuthRoute, NoAuthRoute, routesScheme } from './routing';
 import Sidebar from './shared/Sidebar';
-import { NoMatch } from './shared/ErrorPages';
 import { UserContext } from './core';
-import Authentication from './features/authentication';
-import Profile from './features/profile';
-import { ManagersList, ManagersEditor } from './features/managers';
-import { ClientEditor, ClientList } from './features/clients';
-import { LoansList, LoansEditor, LoansAdd } from './features/loans';
+
+const NoMatch = lazy(() => import('./shared/ErrorPages'));
+const Authentication = lazy(() => import('./features/authentication'));
+const Profile = lazy(() => import('./features/profile'));
+const ManagersList = lazy(() => import('./features/managers').then(m => m.ManagersList));
+const ManagersEditor = lazy(() => import('./features/managers').then(m => m.ManagersEditor));
+const ClientEditor = lazy(() => import('./features/clients').then(m => m.ClientEditor));
+const ClientList = lazy(() => import('./features/clients').then(m => m.ClientList));
+const LoansList = lazy(() => import('./features/loans').then(m => m.LoansList));
+const LoansEditor = lazy(() => import('./features/loans').then(m => m.LoansEditor));
+const LoansAdd = lazy(() => import('./features/loans').then(m => m.LoansAdd));
 
 const App = () => {
   const context = useContext(UserContext);
@@ -21,54 +26,61 @@ const App = () => {
     <>
       <ReactNotification />
       {role && <Sidebar />}
-      <Page>
-        <GlobalStyle />
-        <Switch>
-          <NoAuthRoute exact path={routesScheme.auth} component={Authentication} />
-          <AuthRoute exact path="/" component={App} />
-          <AuthRoute exact path={routesScheme.profile} component={Profile} />
-          <AuthRoleRoute
-            accessRole="admin"
-            exact
-            path={routesScheme.managers}
-            component={ManagersList}
-          />
-          <AuthRoleRoute
-            accessRole="admin"
-            exact
-            path={routesScheme.managersAdd}
-            component={ManagersEditor}
-          />
-          <AuthRoleRoute
-            accessRole="admin"
-            exact
-            path={routesScheme.managersIdRoute}
-            component={ManagersEditor}
-          />
-          <AuthRoute exact path={routesScheme.clients} component={ClientList} />
-          <AuthRoleRoute
-            accessRole="manager"
-            exact
-            path={routesScheme.clientsAdd}
-            component={ClientEditor}
-          />
-          <AuthRoute exact path={routesScheme.clientsIdRoute} component={ClientEditor} />
-          <AuthRoleRoute accessRole="admin" exact path={routesScheme.loans} component={LoansList} />
-          <AuthRoleRoute
-            accessRole="manager"
-            exact
-            path={routesScheme.loansAdd}
-            component={LoansAdd}
-          />
-          <AuthRoleRoute
-            accessRole="admin"
-            exact
-            path={routesScheme.loansIdRoute}
-            component={LoansEditor}
-          />
-          <Route component={NoMatch} />
-        </Switch>
-      </Page>
+      <Suspense fallback={<div>Loading</div>}>
+        <Page>
+          <GlobalStyle />
+          <Switch>
+            <NoAuthRoute exact path={routesScheme.auth} component={Authentication} />
+            <AuthRoute exact path="/" component={App} />
+            <AuthRoute exact path={routesScheme.profile} component={Profile} />
+            <AuthRoleRoute
+              accessRole="admin"
+              exact
+              path={routesScheme.managers}
+              component={ManagersList}
+            />
+            <AuthRoleRoute
+              accessRole="admin"
+              exact
+              path={routesScheme.managersAdd}
+              component={ManagersEditor}
+            />
+            <AuthRoleRoute
+              accessRole="admin"
+              exact
+              path={routesScheme.managersIdRoute}
+              component={ManagersEditor}
+            />
+            <AuthRoute exact path={routesScheme.clients} component={ClientList} />
+            <AuthRoleRoute
+              accessRole="manager"
+              exact
+              path={routesScheme.clientsAdd}
+              component={ClientEditor}
+            />
+            <AuthRoute exact path={routesScheme.clientsIdRoute} component={ClientEditor} />
+            <AuthRoleRoute
+              accessRole="admin"
+              exact
+              path={routesScheme.loans}
+              component={LoansList}
+            />
+            <AuthRoleRoute
+              accessRole="manager"
+              exact
+              path={routesScheme.loansAdd}
+              component={LoansAdd}
+            />
+            <AuthRoleRoute
+              accessRole="admin"
+              exact
+              path={routesScheme.loansIdRoute}
+              component={LoansEditor}
+            />
+            <Route component={NoMatch} />
+          </Switch>
+        </Page>
+      </Suspense>
     </>
   );
 };
